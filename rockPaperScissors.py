84f70fdd516ad2afc656197d19770e3eb87a3fa1
tzvi <zvise@edu.hac.ac.il>
2022-06-14 21:14:16 +0300

    HELLO I FINISHED THE PROJEGT

--- rockPaperScissors.py
+++ rockPaperScissors.py
@@ -48,10 +48,65 @@ print("GAME RULES: \n"
 
 # The game will run in a WHILE loop.
 # The loop is infinite, and only the user has the power to stop it (when they say they don't want to play anymore)
-while True:
-    """
-      This is the game's heart. You'll need to think and use everything we've learned so far to make this game work.
-      Remember Python's rules ( the ':' after a statement, the indentation with loops and statements..)
-      
-      """
-    pass
+gameison = True
+
+while gameison:
+ userchouce =input("choose your choice rock(1),paper(2),scissors(3)")
+
+ while userchouce != "1" and  userchouce != "2" and userchouce != "3":
+    print("invaild choice")
+    userchouce = input("choose your choice rock(1),paper(2),scissors(3)")
+ else:
+     computerchoice=random.randint(1,3)
+     userchouce = int(userchouce)
+
+ #case user choose rock
+
+ if userchouce ==1 and computerchoice == 3:
+      print("yay you won")
+ elif userchouce == 1 and computerchoice ==2:
+      print("damm you loss ")
+ elif userchouce == 1 and computerchoice == 1:
+      print("its a draw!")
+
+
+
+
+
+
+#case user choose paper
+ if userchouce == 2 and computerchoice == 1:
+      print("yay you won")
+ elif userchouce == 2 and computerchoice ==3:
+      print("damm you loss ")
+ elif userchouce == 2 and computerchoice == 2:
+      print("its a draw!")
+
+
+
+
+
+
+#case user choose scissors
+ if userchouce ==3 and computerchoice == 2:
+      print("yay you won")
+ elif userchouce == 3 and computerchoice ==1:
+      print("damm you loss ")
+ elif userchouce == 3 and computerchoice == 3:
+      print("its a draw!")
+
+
+
+ answerreguest = input("wanna continue ? y/n").lower()
+ continuereguest = True
+ while answerreguest != "y" and answerreguest !="n" and continuereguest:
+    print("invalid answer")
+    answerreguest = input("wanna continue ? y/n")
+
+ if answerreguest == "y":
+     continue
+ elif answerreguest == "n":
+     print("thanks for playing the game !")
+     gameison = False
+
+
